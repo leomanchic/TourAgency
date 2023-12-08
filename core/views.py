2959bb7b9baead88dcd94927a04eecae16e4c6d7
leomanchic <perleomix@gmail.com>
2023-12-08 19:03:48 +0300

    Merge remote-tracking branch 'origin/master' search engine edit

--- core/views.py
+++ core/views.py
@@ -36,10 +36,11 @@ def my_account(request):
 def shop(request):
     categories = Category.objects.all()
     products = Product.objects.all()
+    cities = City.objects.all()
 
     active_category = request.GET.get('category', '')
     if active_category:
-        products = products.filter(category__slug=active_category)
+        products = products.filter(category__city__name__contains=active_category)
 
     query = request.GET.get('query', '')
     if query:
@@ -54,6 +55,6 @@ def shop(request):
         'categories': categories,
         'products': products,
         'active_category': active_category,
-        # 'cities': categories[0].city,
+        'cities': cities,
     }
     return render(request, "core/shop.html", context)
